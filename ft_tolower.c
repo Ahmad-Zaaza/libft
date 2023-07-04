f709281ea82eae820e2545aefe8453c4d5d3b28a
Ahmad Rajai Zaaza <azaaza@lab1r4s4.42abudhabi.ae>
2023-07-04 16:14:57 +0400

    add ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 14:51:49 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/04 16:14:01 by azaaza           ###   ########.fr       */
+/*   Created: 2023/07/04 16:14:06 by azaaza            #+#    #+#             */
+/*   Updated: 2023/07/04 16:14:17 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-
-int	ft_isdigit(int c);
-
-int	ft_isalnum(int c);
-
-int	ft_isascii(int c);
-
-int	ft_isprint(int c);
-
-int	ft_strlen(int c);
-
-int	ft_toupper(int c);
-
-int	ft_tolower(int c);
-
-#endif
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
