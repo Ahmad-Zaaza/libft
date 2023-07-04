ac8fb2adbc9418bee857b8c476207f645a4099d3
Ahmad Rajai Zaaza <azaaza@lab1r4s4.42abudhabi.ae>
2023-07-04 15:13:48 +0400

    add ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 14:51:49 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/04 15:13:40 by azaaza           ###   ########.fr       */
+/*   Created: 2023/07/04 15:12:00 by azaaza            #+#    #+#             */
+/*   Updated: 2023/07/04 15:12:39 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_strlen(char *str)
+{
+	int i;
 
-int	ft_isalpha(int c);
-
-int	ft_isdigit(int c);
-
-int	ft_isalnum(int c);
-
-int	ft_isascii(int c);
-
-int	ft_isprint(int c);
-
-int	ft_strlen(int c);
-
-#endif
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
