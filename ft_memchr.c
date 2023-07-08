c88a447cbf8eddc0ea9d6e24f3cb9c5bf4c7c280
Ahmad Rajai Zaaza <azaaza@lab1r4s4.42abudhabi.ae>
2023-07-08 15:07:33 +0400

    complete ft_strlcat.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 13:05:25 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/08 13:22:08 by azaaza           ###   ########.fr       */
+/*   Updated: 2023/07/08 14:01:03 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	int		i;
 	char	*str;
-
+ 
 	i = 0;
 	str = s;
 	while (str[i] && n)
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 14:51:45 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/06 15:59:25 by azaaza           ###   ########.fr       */
+/*   Updated: 2023/07/08 15:07:19 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@ size_t	ft_strlcat(char *restrict dest, char *restrict src, size_t dest_size)
 	size_t	j;
 
 	i = 0;
-	j = dest_size;
-	while (src[i] && i < dest_size)
+	j = 0;
+	while (dest[j])
+		j++;
+	while (src[i] && j < (dest_size - 1))
 	{
 		dest[j] = src[i];
 		j++;
 		i++;
+	}
+	while (j < (dest_size - 1))
+	{
+		dest[j] = '\0';
+		j++;
 	}
 	dest[j] = '\0';
 	return (j);
@@ -31,14 +38,15 @@ size_t	ft_strlcat(char *restrict dest, char *restrict src, size_t dest_size)
 #include <stdio.h>
 #include <string.h>
 
-int	main(void)
-{
-	char str[50] = " World";
-	char str2[50] = "Hello";
+// int	main(void)
+// {
+// 	char str[50] = " World";
+// 	char str2[12] = "Hello";
 
-	printf("Ours: %lu\n", ft_strlcat(str2, str, 10));
-	printf("String after: %s\n", str2);
-	printf("Original: %lu\n", strlcat(str2, str, 10));
+// 	printf("Ours: %lu\n", ft_strlcat(str2, str, 12));
+// 	printf("String after: %s\n", str2);
+// 	// printf("Original: %lu\n", strlcat(str2, str, 12));
+// 	// printf("String after: %s\n", str2);
 
-	return (0);
-}
+// 	return (0);
+// }
