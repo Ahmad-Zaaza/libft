4950cf2e83469f416641d86dcfde0c6360ba2079
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2023-07-10 12:16:23 +0400

    fix ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,12 +17,13 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]) && i < n)
+	while (i < n)
 	{
 		if ((((unsigned char *)s1)[i]) > (((unsigned char *)s2)[i]))
 			return (1);
 		else if ((((unsigned char *)s1)[i]) < (((unsigned char *)s2)[i]))
 			return (-1);
+
 		i++;
 	}
 	return (0);
