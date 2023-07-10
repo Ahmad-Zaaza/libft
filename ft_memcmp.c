7f63f6d75008466c53de6954f1a05fab547b8378
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2023-07-10 11:39:28 +0400

    fix ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,19 +12,18 @@
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	while (n)
+	size_t i;
+
+	i = 0;
+	while ((((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]) && i < n)
 	{
-		if (((unsigned char *)s1) > ((unsigned char *)s2))
-		{
-			return (((unsigned char *)s1) - ((unsigned char *)s2));
-		}
-		else if (((unsigned char *)s1) < ((unsigned char *)s2))
-		{
-			return (((unsigned char *)s2) - ((unsigned char *)s1));
-		}
-		n--;
+		if ((((unsigned char *)s1)[i]) > (((unsigned char *)s2)[i]))
+			return (1);
+		else if ((((unsigned char *)s1)[i]) < (((unsigned char *)s2)[i]))
+			return (-1);
+		i++;
 	}
 	return (0);
 }
