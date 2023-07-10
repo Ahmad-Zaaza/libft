cf4b2a0d59f78f20fff8563f4729e736e125e12e
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2023-07-10 12:42:14 +0400

    fix ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,18 +12,16 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while (((unsigned char *)s)[i] && i < n)
+	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
 			return ((void *)((unsigned char *)s) + i);
 		i++;
 	}
-	if (((unsigned char *)s)[i] == (unsigned char)c && i < n)
-		return (((void *)(unsigned char *)s) + i);
 	return (NULL);
 }
