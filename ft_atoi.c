cf6293ca47a66e37cae477de66159390ea316595
Ahmad Rajai Zaaza <azaaza@lab1r4s4.42abudhabi.ae>
2023-07-11 17:21:39 +0400

    more work on atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 16:15:59 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/11 16:40:23 by azaaza           ###   ########.fr       */
+/*   Updated: 2023/07/11 17:20:08 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,21 @@ int	ft_atoi(const char *str)
 	long int	result;
 	int			i;
 	int			sign;
+	int			count;
 
 	result = 0;
 	i = 0;
 	sign = 1;
+	count = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i] == 45)
-	{
+	if (str[i++] == 45)
 		sign = -1;
-		i++;
-	}
 	else if (str[i] == 43)
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (result * 10 > 2147483647)
+		if (count++ == 19)
 		{
 			if (sign > 0)
 				return (-1);
@@ -38,20 +37,17 @@ int	ft_atoi(const char *str)
 				return (0);
 		}
 		else
-		{
-			result = (result * 10) + (str[i] - 48);
-			i++;
-		}
+			result = (result * 10) + (str[i++] - 48);
 	}
 	return (result * sign);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int	main(void)
-{
-	char *str = "       +429496729542949672954294967295";
-	printf("%d\n", ft_atoi(str));
-	printf("%d\n", atoi(str));
-}
+// int	main(void)
+// {
+// 	char *str = "92233720368547758070";
+// 	printf("%d\n", ft_atoi(str));
+// 	printf("%d\n", atoi(str));
+// }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:14:37 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/11 16:30:56 by azaaza           ###   ########.fr       */
+/*   Updated: 2023/07/11 17:10:40 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,32 @@ static	is_match(char const *set, char c)
 	return (0);
 }
 
-static	get_len(char const *s1, char const *set)
+static int	get_len(char const *s1, char const *set)
 {
 	int	i;
 	int	len;
+	int	is_last;
 
 	i = 0;
 	len = 0;
+	is_last = 1;
+	// while (s1[i])
+	// {
+	// 	if (!is_match(set, s1[i]))
+	// 	{
+	// 	}
+	// }
 	while (is_match(set, s1[i]))
+		i++;
+	while (s1[i] && !is_match(set, s1[i]))
 	{
 		i++;
+		len++;
 	}
+	while (is_match(set, s1[i]))
+		i++;
+	is_last = 1;
+	return (len);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
