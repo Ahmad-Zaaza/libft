b056f5c79fbc863850a5d4d701a0f61072a3c575
Ahmad Rajai Zaaza <azaaza@lab1r4s3.42abudhabi.ae>
2023-07-15 15:01:04 +0400

    add ft_lstadd_back.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 21:08:43 by ahmadzaaza        #+#    #+#             */
-/*   Updated: 2023/07/15 14:45:57 by azaaza           ###   ########.fr       */
+/*   Updated: 2023/07/15 14:49:33 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,5 +99,7 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 
 t_list				*ft_lstlast(t_list *lst);
+
+void				ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
