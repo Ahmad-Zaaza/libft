d5ea2e79e4a1355877f809555c141d1ae53019d9
Ahmad Rajai Zaaza <azaaza@lab1r4s4.42abudhabi.ae>
2023-07-18 11:33:25 +0400

    fix ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,51 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahmadzaaza <ahmadzaaza@student.42.fr>      +#+  +:+       +#+        */
+/*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 16:15:59 by azaaza            #+#    #+#             */
-/*   Updated: 2023/07/14 21:11:56 by ahmadzaaza       ###   ########.fr       */
+/*   Updated: 2023/07/18 11:32:14 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
 	long int	result;
+	long int	old_result;
 	int			i;
 	int			sign;
-	int			count;
 
 	result = 0;
 	i = 0;
 	sign = 1;
-	count = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i++] == 45)
+	if (str[i] == 45)
 		sign = -1;
-	else if (str[i] == 43)
-		i++;
+	i += (str[i] == '-' || str[i] == '+');
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (count++ == 19)
+		old_result = result;
+		result = (result * 10) + (str[i++] - 48);
+		if (old_result > result)
 		{
-			if (sign > 0)
-				return (-1);
-			else if (sign < 0)
+			if (sign < 0)
 				return (0);
+			return (-1);
 		}
-		else
-			result = (result * 10) + (str[i++] - 48);
 	}
 	return (result * sign);
 }
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int	main(void)
-// {
-// 	char *str = "92233720368547758070";
-// 	printf("%d\n", ft_atoi(str));
-// 	printf("%d\n", atoi(str));
-// }
